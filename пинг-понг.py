c664139d2b0df495d2deb9550051e7bd30c15edc
GolubevaAnastasia <134947776+GolubevaAnastasia@users.noreply.github.com>
2023-06-02 17:20:59 +0300

    Add files via upload

---
+++ пинг-понг.py
@@ -0,0 +1,72 @@
+#Создай собственный Шутер!
+from pygame import *
+from random import randint
+from time import time as timer
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed,s1,s2):
+        super().__init__()
+        self.image=transform.scale(image.load(player_image), (s1, s2))
+        self.speed=player_speed
+        self.rect=self.image.get_rect()
+        self.rect.x=player_x
+        self.rect.y=player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_r(self):
+        keys=key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y-=self.speed
+        if keys[K_DOWN] and self.rect.y < 500 - 80:
+            self.rect.y+=self.speed        
+    def update_l(self):
+        keys=key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y-=self.speed
+        if keys[K_s] and self.rect.y < 500 - 80:
+            self.rect.y+=self.speed       
+
+window=display.set_mode((700, 500)) 
+display.set_caption('pygame window')
+ball=GameSprite('ball.png', 150, 20, 0, 65, 65)
+sprite1=Player("rac.png", 5, 40, 15, 20, 100)
+sprite2=Player("rac.png", 500, 150, 60, 20, 100)
+clock=time.Clock()
+FPS=60
+game=True
+finish=False
+speed_x=3
+speed_y=3
+font.init()
+font=font.Font(None, 66)
+lose1=font.render('Проиграл первый игрок', True, (0, 0, 0))
+lose2=font.render('Проиграл второй игрок', True, (0, 0, 0))
+while game:
+    for e in event.get():
+        if e.type==QUIT:
+            game=False
+    if not finish:
+        window.fill((255, 255, 255))
+        ball.reset()
+        sprite1.reset()
+        sprite2.reset()
+        sprite1.update_r()
+        sprite2.update_l()
+        ball.rect.x*=speed_x
+        ball.rect.y*=speed_y
+        
+        if ball.rect.y > 500 or ball.rect.y<0:
+            speed_y*=-1
+        if sprite.collide_rect(sprite1, ball) or sprite.collide_rect(sprite2, ball):
+            speed_x*=-1
+        if ball.rect.x<0:
+            finish=True
+            win.blit(lose1, (100, 50))
+        if ball.rect.x>1000:
+            finish=True
+            window.blit(lose2, (100, 50))
+
+        display.update()
+        clock.tick(60)
